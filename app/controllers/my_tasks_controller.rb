c4b49d7ce813eff23ead3c3de64a950f0b840d91
ShirinValizadeh <shvalizadeh2020@gmail.com>
2021-02-12 13:20:42 +0100

    task done

--- app/controllers/my_tasks_controller.rb
+++ app/controllers/my_tasks_controller.rb
@@ -1,5 +1,5 @@
 class MyTasksController < ApplicationController
-    before_action :set_my_task, only: [:update]
+    before_action :set_my_task, only: [:update , :destroy , :show]
 
     def index
        @my_tasks= Task.where(assignee_id: current_user.id).all
@@ -7,11 +7,24 @@ class MyTasksController < ApplicationController
 
 
       def update 
+        
         if @my_task.update(task_params)
            redirect_to my_tasks_url, notice: 'Task was successfully updated.'         
         else
            render :edit  
         end     
+    end
+
+
+    # DELETE /tasks/1
+    def destroy
+      @my_task.destroy
+      redirect_to my_tasks_url, notice: 'Task was successfully destroyed.' 
+    end
+
+
+    def show
+  
     end
 
     private
@@ -22,7 +35,7 @@ class MyTasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:my_task).permit(:title , :assignee_id)
+      params.require(:my_task).permit(:title , :assignee_id , :completed)
     end
 
 
