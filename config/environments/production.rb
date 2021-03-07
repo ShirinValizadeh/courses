17806ac82ed73f8c1eb1870797b3d0e8ddf052af
ShirinValizadeh <shvalizadeh2020@gmail.com>
2021-03-07 13:49:48 +0100

    config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV["RENDER"].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
@@ -129,4 +129,9 @@ Rails.application.configure do
    config.action_mailer.default_url_options = { host: 'course-app-rails.herokuapp.com' }
 
 
+   Raven.configure do |config|
+    config.dsn = ENV["SENTRY_IO_DSN"]
+  end
+
+  config.exceptions_app = self.routes
 end
