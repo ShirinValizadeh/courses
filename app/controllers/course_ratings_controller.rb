b645c4fc38d02ddcea414013f6c44cfdeb70ac13
ShirinValizadeh <shvalizadeh2020@gmail.com>
2021-03-15 16:54:40 +0100

    fix rating 2

--- app/controllers/course_ratings_controller.rb
+++ app/controllers/course_ratings_controller.rb
@@ -1,18 +1,8 @@
 class CourseRatingsController < ApplicationController
-  # before_action :set_course_rating, only: %i[show edit update]
+
   before_action :set_course
 
-  # def index
-  #   @course_ratings = CourseRating.all
-  # end
 
-  # def show; end
-
-  # def new
-  #   @course_rating = CourseRating.new
-  # end
-
-  # def edit; end
 
   def create
     @course_rating = CourseRating.new(course_rating_params)
@@ -24,20 +14,9 @@ class CourseRatingsController < ApplicationController
     end
   end
 
-  # def update
-  #   if @course_rating.update(course_rating_params)
-  #     redirect_to @course, notice: 'Course rating was successfully updated.'
-  #   else
-  #     render :edit
-
-  #   end
-  # end
 
   private
 
-  # def set_course_rating
-  #   @course_rating = CourseRating.find(params[:id])
-  # end
 
   def set_course
     @course = Course.find(course_rating_params[:course_id])
