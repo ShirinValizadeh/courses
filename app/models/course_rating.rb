4c03c540f315a2d4da72781242f496f31c66e472
ShirinValizadeh <shvalizadeh2020@gmail.com>
2021-03-16 12:09:38 +0100

    production rb smtp

--- app/models/course_rating.rb
+++ app/models/course_rating.rb
@@ -1,6 +1,6 @@
 class CourseRating < ApplicationRecord
   belongs_to :course
-  #validates :rating, numericality: { greater_than: 0, less_than_or_equal_to: 5 }
+  validates :rating, numericality: { greater_than: 0, less_than_or_equal_to: 5 }
   
   def blank_stars
     5 - rating.to_i
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "curses_production"
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.default_url_options = { host: 'course-app-rails.herokuapp.com' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    domain:               'course-app-rails.com',
+    port:                 587,
+    user_name:            ENV["GMAIL_USER"],
+    password:             ENV["GMAIL_API_KEY"],
+    authentication:       "plain",
+    enable_starttls_auto: true
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
@@ -129,7 +140,7 @@ Rails.application.configure do
 
 
   
-  # config.action_mailer.default_url_options = { host: 'course-app-rails.herokuapp.com' }
+  
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
  
