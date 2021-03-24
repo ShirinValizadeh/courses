167d34681507b56e707529798c97fb7ccd2e8691
ShirinValizadeh <shvalizadeh2020@gmail.com>
2021-03-24 11:29:34 +0100

    posting controller test

--- test/controllers/api/postings_controller_test.rb
+++ test/controllers/api/postings_controller_test.rb
@@ -14,33 +14,31 @@ class Api::PostingsControllerTest < ActionDispatch::IntegrationTest
       end
 
 
-    # test "should show posting" do
-    #    get api_posting_url(@posting.id)
-    #    assert_response :success
-    # end
+    test "should show posting" do
+       get api_posting_url(@posting.id),headers:{Authorization: "Bearer abc123" }
+       assert_response :success
+    end
       
-    # test "should create posting" do  
-    #     assert_difference('Posting.count') do     
-    #       post api_postings_url,
-    #             params: { posting: { description: @posting.description, 
-    #                     title: @posting.title } }
-    #             end   
-    #     assert_response :success
-    #    end
+    test "should create posting" do  
+        assert_difference('Posting.count') do  
+          post api_postings_url,headers:{Authorization: "Bearer abc123" },
+                params: { posting: { description: @posting.description, 
+                        title: @posting.title } }
+                end   
+        assert_response :success
+       end
 
 
-    # test "should destroy posting" do
-    #     assert_difference("Posting.count", -1) do
-    #     delete api_posting_url(@posting.id)
-    #     assert_response :success
-    #     end
-    # end
+    test "should destroy posting" do
+        assert_difference("Posting.count", -1) do
+        delete api_posting_url(@posting.id),headers:{Authorization: "Bearer abc123" }
+        assert_response :success
+        end
+    end
 
-    # test "should update posting" do
-    #     patch api_posting_url(@posting.id), params: { posting: { description: description, title: "new title" } }
-    #     assert_response :success
-    #     json = JSON.parse response.body
-    #     assert_equal [], json["body"]["blocks"]
-    #     assert_equal "new title", json["title"]
-    #   end
+    test "should update posting" do
+        patch api_posting_url(@posting.id),headers:{Authorization: "Bearer abc123" }, params: { posting: { description: @posting.description, title: @posting.title } }
+        assert_response :success
+
+      end
 end    
